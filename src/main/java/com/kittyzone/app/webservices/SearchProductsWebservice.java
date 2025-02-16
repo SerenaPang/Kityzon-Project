85b5f56f57c906b5219d659c8505bc5b4dcc4844
SerenaPang4 <ypang4@usfca.edu>
2025-02-16 13:43:02 -0800

    Autowired ProductService in SearchProductsWebservice and handle searches

--- src/main/java/com/kittyzone/app/webservices/SearchProductsWebservice.java
+++ src/main/java/com/kittyzone/app/webservices/SearchProductsWebservice.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,35 +12,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kittyzone.app.model.Product;
+import com.kittyzone.app.service.ProductsService;
 
 @RestController
 public class SearchProductsWebservice {
+
+	@Autowired
+	private ProductsService productService;
 
 	// curl -X GET "http://localhost:8080/searchByQuery?query=myproduct"
 	@GetMapping("/searchByQuery")
 	public ResponseEntity<List<Product>> searchByQuery(@RequestParam(value = "query", defaultValue = "nothing") String query) {
 		System.out.println("SearchProductsWebservice.searchByQuery() " + query);
-		List<Product> products = dummyProducts();
+		List<Product> products = productService.searchBySearchString(query);
 
 		return ResponseEntity.status(HttpStatus.OK).body(products);
-	}
-
-	/*
-	 * Return dummy products.
-	 */
-	private List<Product> dummyProducts() {
-		List<Product> products = new ArrayList<>();
-
-		for (int i = 1; i < 11; i++) {
-			Product product = new Product();
-			product.setId(i);
-			product.setName("Cat tree " + i);
-			product.setBrand("XinTu " + i);
-			product.setDescription("Cat tree for medium size cats " + i);
-			product.setPrice(new BigDecimal(23.52));
-			products.add(product);
-		}
-
-		return products;
 	}
 }
